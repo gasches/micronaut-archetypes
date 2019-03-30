c8361a3cbfab22d0ab72bfea2df382a38f0d332d
Semyon Gashchenko <semyon.gashchenko@gmail.com>
2019-03-30 22:57:00 +0300

    Add kotlin cli

---
+++ micronaut-kotlin-cli/src/main/resources/archetype-resources/src/main/kotlin/Application.kt
@@ -0,0 +1,31 @@
+package ${package}
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "${artifactId}",
+        description = ["..."],
+        mixinStandardHelpOptions = true)
+class Application : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(Application::class.java, *args)
+        }
+    }
+}
