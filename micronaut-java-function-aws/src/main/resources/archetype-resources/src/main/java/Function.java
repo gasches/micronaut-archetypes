d1469522ee71ea83cba0dd39b4a88d3f80ceec2d
Semyon Gashchenko <semyon.gashchenko@gmail.com>
2019-03-31 13:14:02 +0300

    Fix java aws function archetype

--- micronaut-java-function-aws/src/main/resources/archetype-resources/src/main/java/Function.java
+++ micronaut-java-function-aws/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -13,7 +13,7 @@ public class Function extends FunctionInitializer {
     }
 
     /**
-     * This main method allows running the function as a CLI application using: echo '{}' | java -jar function.jar
+     * This main method allows running the function as a CLI application using: {@code echo '{}' | java -jar ${artifactId}.jar}
      * where the argument to echo is the JSON to be parsed.
      */
     public static void main(String...args) throws IOException {
